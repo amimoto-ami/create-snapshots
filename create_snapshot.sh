6d10b00b87260c0ac601e2889a399e75a76b2ba3
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2015-04-29 00:30:27 +0900

    Update create_snapshot.sh

--- create_snapshot.sh
+++ create_snapshot.sh
@@ -17,24 +17,6 @@ AWS="/usr/bin/aws --region ${REGION}"
 
 INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 
-rotate_log() {
-    (( cnt=${LOG_SAVE_PERIOD} ))
-    while (( cnt > 0 ))
-    do
-        logfile1=${LOG_FILE}.$cnt
-        (( cnt=cnt-1 ))
-        logfile2=${LOG_FILE}.$cnt
-        if [ -f $logfile2 ]; then
-            mv $logfile2 $logfile1
-        fi
-    done
-
-    if [ -f $LOG_FILE ]; then
-        mv ${LOG_FILE} ${LOG_FILE}.1
-    fi
-    touch $LOG_FILE
-}
-
 print_msg() {
     echo "`date '+%Y/%m/%d %H:%M:%S'` $1" | tee -a ${LOG_FILE}
 }
@@ -45,14 +27,12 @@ create_snapshot() {
     if [ -z ${VOL_ID} ] ; then
         echo ${VOL_ID}
         print_msg "ERR:ec2-describe-instances"
-        logger -f ${LOG_FILE}
         exit 1
     fi
     print_msg "ec2-describe-instances Success : ${VOL_ID}"
     ${AWS} ec2 create-snapshot --volume-id ${VOL_ID} --description "Created by SYSTEMBK(${INSTANCE_ID}) from ${VOL_ID}" >> ${LOG_FILE} 2>&1
     if [ $? != 0 ] ; then
         print_msg "ERR:${SHELLDIR}/${SHELLNAME} ec2-create-snapshot"
-        logger -f ${LOG_FILE}
         exit 1
     fi
     print_msg "Create snapshot End"
@@ -66,15 +46,12 @@ delete_old_snapshot() {
         ${AWS} ec2 delete-snapshot --snapshot-id `${AWS} ec2 describe-snapshots --output text | grep ${VOL_ID} | grep "Created by SYSTEMBK" | sort -k 11,11 | awk 'NR==1 {print $10}'` >> ${LOG_FILE} 2>&1
         if [ $? != 0 ] ; then
             print_msg "ERR:${SHELLDIR}/${SHELLNAME} ec2-delete-snapshot"
-            logger -f ${LOG_FILE}
             exit 1
         fi
         SNAPSHOTS=`${AWS} ec2 describe-snapshots | grep ${VOL_ID} | grep "Created by SYSTEMBK" | wc -l`
     done
     print_msg "Delete old snapshot End"
 }
-
-rotate_log
 
 print_msg "INF:$SHELLDIR/${SHELLNAME} START"
 create_snapshot
