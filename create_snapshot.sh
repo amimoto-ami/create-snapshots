2393ebb4d8b96e49285cb65d561bf456a8689e52
miya0001 <miya@wpist.me>
2015-07-06 23:20:35 +0900

    some fix

--- create_snapshot.sh
+++ create_snapshot.sh
@@ -16,30 +16,37 @@ AWS="/usr/bin/aws --region ${REGION}"
 INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 INSTANCE_NAME=`${AWS} ec2 describe-instances --instance-ids ${INSTANCE_ID} --output json | jq -r '.Reservations[].Instances[].Tags[] | select(.Key == "Name").Value'`
 
-print_msg() {
-    echo "`date '+%Y/%m/%d %H:%M:%S'` $1"
+if [ $? != 0 ] ; then
+    error 'Unable to locate credentials. You can configure credentials by running "aws configure".'
+    exit 1
+fi
+
+error() {
+    echo -e "\e[31m${@}\e[m"
+}
+
+success() {
+    echo -e "\e[32m${@}\e[m"
 }
 
 create_snapshot() {
     print_msg "Create snapshot Start"
     VOL_ID=`${AWS} ec2 describe-instances --instance-ids ${INSTANCE_ID} --output text | grep EBS | awk '{print $5}'`
     if [ -z ${VOL_ID} ] ; then
-        echo ${VOL_ID}
-        print_msg "ERR:ec2-describe-instances"
+        error "ERR: ec2-describe-instances"
         exit 1
     fi
     print_msg "ec2-describe-instances Success : ${VOL_ID}"
     SNAPSHOT_ID=`${AWS} ec2 create-snapshot --volume-id ${VOL_ID} --description "Created by SYSTEMBK(${INSTANCE_ID}) from ${VOL_ID}" --output json | jq -r .SnapshotId`
     if [ $? != 0 ] ; then
-        print_msg "ERR: ec2-create-snapshot"
+        error "ERR: ec2-create-snapshot"
         exit 1
     fi
     ${AWS} ec2 create-tags --resources ${SNAPSHOT_ID} --tags "Key=Name,Value=${INSTANCE_NAME}-$(date '+%Y/%m/%d')"
     if [ $? != 0 ] ; then
-        print_msg "ERR: ec2-create-tags"
+        error "ERR: ec2-create-tags"
         exit 1
     fi
-    print_msg "Create snapshot End"
 }
 
 delete_old_snapshot() {
@@ -49,12 +56,11 @@ delete_old_snapshot() {
     do
         ${AWS} ec2 delete-snapshot --snapshot-id `${AWS} ec2 describe-snapshots --output text | grep ${VOL_ID} | grep "Created by SYSTEMBK" | sort -k 11,11 | awk 'NR==1 {print $10}'`
         if [ $? != 0 ] ; then
-            print_msg "ERR:${SHELLDIR}/${SHELLNAME} ec2-delete-snapshot"
+            error "ERR: ${SHELLDIR}/${SHELLNAME} ec2-delete-snapshot"
             exit 1
         fi
         SNAPSHOTS=`${AWS} ec2 describe-snapshots | grep ${VOL_ID} | grep "Created by SYSTEMBK" | wc -l`
     done
-    print_msg "Delete old snapshot End"
 }
 
 sudo yum install jq
